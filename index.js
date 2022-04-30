58a57e7ee11a1630a0792effc14478589bd3cafd
Imran Hossen <freelancerimran81@gmail.com>
2022-04-30 23:01:46 +0600

    database server setup

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+
+
+
+// ShoesWarehouse
+// rNvA7O7Vmr8NhVOD
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfcfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running my code CRUD SERVER')
+});
+app.listen(port,()=>{
+    console.log('Crud server is running');
+})
