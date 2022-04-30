126f4005dcc671e5580d079f4fdd4dc4e052358b
Imran Hossen <freelancerimran81@gmail.com>
2022-04-30 23:03:23 +0600

    update

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ require('dotenv').config();
 
 
 
-// ShoesWarehouse
-// rNvA7O7Vmr8NhVOD
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfcfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
