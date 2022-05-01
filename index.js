e932cbf725dacf1132f85bc50db235cb1853b78e
Imran Hossen <freelancerimran81@gmail.com>
2022-05-01 11:00:14 +0600

    get and post method done

--- index.js
+++ index.js
@@ -7,17 +7,76 @@ app.use(cors());
 app.use(express.json());
 require('dotenv').config();
 
+// DB_USER=ShoesWarehouse
+// DB_PASS=rNvA7O7Vmr8NhVOD
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfcfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://ShoesWarehouse:rNvA7O7Vmr8NhVOD@cluster0.gfcfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db("shoesWarehouse").collection("shoes");
+
+        // get api to read all inventories product
+
+        app.get("/shoes", async (req, res)=>{
+            const query = req.query;
+            console.log(query);
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+
+            res.send(result)
+        })
+
+        // Create inventories product
+        // localhost:5000/shoe
+
+        app.post('/shoe', async (req, res) => {
+            const data = req.body;
+            // console.log(data);
+            const result = await productCollection.insertOne(data);
+            res.send(result);
+        })
+
+        // Update inventories product
+
+        app.put('/shoe/:id', async(req, res)=>{
+            const id = req.params.id;
+            const data = req.body;
+            const filter = {_id: ObjectId(id)};
+            const option = {upsert:true};
+            const updateDoc = {
+                $set:{
+                    quantity: data.quantity
+                }
+            };
+            const result = await productCollection.updateOne(filter, updateDoc, options);
+
+            res.send(result)
+        })
+
+
+
+        // Delete inventories product
+        console.log('connected to db');
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+
+/* client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
-});
+  console.log('connected to database')
+}); */
 
 
 
